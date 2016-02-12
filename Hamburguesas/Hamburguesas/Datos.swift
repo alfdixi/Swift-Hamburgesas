37efaaff10525da80d09b597800a6b0294ff0eeb
JACS (J. Alfredo Castillejos Sánchez) <alfdixi@gmail.com>
2016-02-12 04:59:39 -0600

    Se suben los 20 nombres solicitados.

--- Hamburguesas/Hamburguesas/Datos.swift
+++ Hamburguesas/Hamburguesas/Datos.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 class ColeccionDePaises{
-    var paises : [String] = ["México","EUA","España","Inglaterra","Francia","Alemania","China"]
+    var paises : [String] = ["México","EUA","España","Inglaterra","Francia","Alemania","China","Austria","Korea del Sur","Japon","Brasil","Canada","Italia","Tailandia","Chile","Colombia","Israel","Alaska","Cuba","Peru"]
     func obtenPais() -> String{
         let numeroAleatorio = Int(arc4random()) % paises.count
         return paises[numeroAleatorio]
@@ -19,7 +19,7 @@ class ColeccionDePaises{
 }
 
 class ColeccionDeHamburguesa{
-    var hamburguesas : [String] = ["Chica","Peque","Mediana","Grante","Total","Super","Plus"]
+    var hamburguesas : [String] = ["Chica","Peque","Mediana","Grante","Total","Super","Plus","Ultra","Perrona","Extra","Grandiosa","No te la acabas","Extra grande","Super extra mega plus","Gran Hamburger","Sin queso","Con extra queso","La suprema","MAgnifica","Esquisita"]
     func obtenHamburguesa() -> String{
         let numeroAleatorio = Int(arc4random()) % hamburguesas.count
         return hamburguesas[numeroAleatorio]
